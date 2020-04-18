574ff2af7c7b08a7684e080e07e7044f35ab61f4
Sam Brodersen <sam.talebi84@gmail.com>
2020-04-18 17:34:46 +0200

    finish setting up reducers, actions and store

--- client/src/store.js
+++ client/src/store.js
@@ -2,12 +2,13 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 //middleware for Redux that allows us to directly access the dispatch method to make asynchronous calls from our actions
 import thunk from 'redux-thunk';
+import rootReducer from './reducers';
 
 const initialState = {};
 const middleware = [thunk];
 
 const store = createStore(
-  () => [],
+  rootReducer,
   initialState,
   compose(
     applyMiddleware(...middleware),
